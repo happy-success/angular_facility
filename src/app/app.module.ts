b78f806484af97f1902c960c880fca2941819f81
jwrunner3224 <joybrother2019@gmail.com>
2020-05-14 05:07:51 +0300

    structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MainpageComponent } from './mainpage/mainpage.component';
+import { DescriptionComponent } from './mainpage/description/description.component';
+import { AddressComponent } from './mainpage/address/address.component';
+import { ImagesComponent } from './mainpage/images/images.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainpageComponent,
+    DescriptionComponent,
+    AddressComponent,
+    ImagesComponent
   ],
   imports: [
     BrowserModule,
